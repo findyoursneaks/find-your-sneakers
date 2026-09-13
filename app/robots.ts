064cd0055ea9a9f35931f266831a2b925647aece
findyoursneaks <andrefsimoes13@gmail.com>
2026-09-13 02:05:26 +0200

    Point robots to solewar.com

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/go/'] }],
-    sitemap: 'https://findyoursneakers.com/sitemap.xml',
+    sitemap: 'https://solewar.com/sitemap.xml',
   };
 }
