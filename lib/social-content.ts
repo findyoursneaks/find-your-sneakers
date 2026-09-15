8c0363ab75ab3bb7346da36eacc58eb9ec2bc405
findyoursneaks <andrefsimoes13@gmail.com>
2026-09-14 20:26:34 -0400

    Register Solewar social video assets

---
+++ lib/social-content.ts
@@ -0,0 +1,7 @@
+export const socialVideoAssets = [
+  "/media/social/solewar-airmax95-deal.mp4",
+  "/media/social/solewar-samba-spotlight.mp4",
+  "/media/social/solewar-af1-cop-or-drop.mp4",
+  "/media/social/solewar-top3-under-150.mp4",
+  "/media/social/solewar-this-or-that.mp4",
+] as const;
